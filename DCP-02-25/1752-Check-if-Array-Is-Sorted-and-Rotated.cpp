b980abc9cf0beacaeb9a5880bf6e1c6558a4eedf
Pothamsetti Sai Srujan Reddy <157013684+SaiSrujanReddyP@users.noreply.github.com>
2025-02-02 22:42:31 +0530

    [1752] [Time Beats: 100.00%] [Memory Beats: 22.96%] - LeetPush

---
+++ DCP-02-25/1752-Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+class Solution {
+public:
+    bool check(std::vector<int>& nums) {
+        int count = 0, n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            if (count > 1) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
