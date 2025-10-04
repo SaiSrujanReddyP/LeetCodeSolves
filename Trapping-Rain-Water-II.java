94a84ef20a9bc4b1213cc09a713244ec9d6c9bc9
Pothamsetti Sai Srujan Reddy <157013684+SaiSrujanReddyP@users.noreply.github.com>
2025-10-04 21:59:27 +0530

    [407] [Time Beats: 28.20%] [Memory Beats: 24.80%] - LeetPush

---
+++ Trapping-Rain-Water-II.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        if (m <= 2 || n <= 2) return 0;
+
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+
+        for (int i = 0; i < m; i++) {
+            pq.offer(new int[]{i, 0, heightMap[i][0]});
+            pq.offer(new int[]{i, n - 1, heightMap[i][n - 1]});
+            visited[i][0] = visited[i][n - 1] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            pq.offer(new int[]{0, j, heightMap[0][j]});
+            pq.offer(new int[]{m - 1, j, heightMap[m - 1][j]});
+            visited[0][j] = visited[m - 1][j] = true;
+        }
+
+        int trappedWater = 0;
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!pq.isEmpty()) {
+            int[] cell = pq.poll();
+            int x = cell[0], y = cell[1], height = cell[2];
+
+            for (int[] d : dirs) {
+                int nx = x + d[0], ny = y + d[1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n || visited[nx][ny]) continue;
+
+                visited[nx][ny] = true;
+                int neighborHeight = heightMap[nx][ny];
+
+                if (neighborHeight < height)
+                    trappedWater += height - neighborHeight;
+
+                pq.offer(new int[]{nx, ny, Math.max(neighborHeight, height)});
+            }
+        }
+
+        return trappedWater;
+    }
+}
