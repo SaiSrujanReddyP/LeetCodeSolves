a2a12eee1895435ec13c5481cac884fabfb56d11
Pothamsetti Sai Srujan Reddy <157013684+SaiSrujanReddyP@users.noreply.github.com>
2025-10-04 13:06:29 +0530

    [3100] [Time Beats: 100.00%] [Memory Beats: 19.21%] - LeetPush

---
+++ Water-Bottles-II.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int empty = 0;
+        int totalDrunk = 0;
+
+        while (numBottles > 0) {
+            totalDrunk += numBottles;
+            empty += numBottles;
+            numBottles = 0;
+
+            if (empty >= numExchange) {
+                empty -= numExchange;   
+                numBottles = 1;        
+                numExchange++;         
+            } else {
+                break;
+            }
+        }
+
+        return totalDrunk;
+    }
+}
