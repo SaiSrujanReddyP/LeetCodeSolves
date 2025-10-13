2a56f0e433ab389f1696bf2ba28fc769ada9be76
Pothamsetti Sai Srujan Reddy <157013684+SaiSrujanReddyP@users.noreply.github.com>
2025-10-14 00:04:46 +0530

    [2273] [Time Beats: 93.48%] [Memory Beats: 82.79%] - LeetPush

---
+++ DCP-10-25/Find-Resultant-Array-After-Removing-Anagrams.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> result = new ArrayList<>();
+        result.add(words[0]);
+        
+        for (int i = 1; i < words.length; i++) {
+            String prev = result.get(result.size() - 1);
+            if (!isAnagram(prev, words[i])) {
+                result.add(words[i]);
+            }
+        }
+        
+        return result;
+    }
+    
+    private boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        int[] count = new int[26];
+        for (char c : s1.toCharArray()) count[c - 'a']++;
+        for (char c : s2.toCharArray()) count[c - 'a']--;
+        for (int n : count) if (n != 0) return false;
+        return true;
+    }
+}
