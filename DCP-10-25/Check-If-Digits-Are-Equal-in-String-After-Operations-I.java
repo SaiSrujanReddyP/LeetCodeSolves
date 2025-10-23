3c07978a81d8afe4a35adb941cea36cea5bfa5d8
Pothamsetti Sai Srujan Reddy <157013684+SaiSrujanReddyP@users.noreply.github.com>
2025-10-23 18:44:55 +0530

    [3461] [Time Beats: 72.03%] [Memory Beats: 69.77%] - LeetPush

---
+++ DCP-10-25/Check-If-Digits-Are-Equal-in-String-After-Operations-I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasSameDigits(String s) {
+        while (s.length() > 2) {
+            StringBuilder newString = new StringBuilder();
+            for (int i = 0; i < s.length() - 1; i++) {
+                int first = s.charAt(i) - '0';
+                int second = s.charAt(i + 1) - '0';
+                int sum = (first + second) % 10;
+                newString.append(sum);
+            }
+            s = newString.toString();
+        }
+        return s.charAt(0) == s.charAt(1);
+    }
+}
