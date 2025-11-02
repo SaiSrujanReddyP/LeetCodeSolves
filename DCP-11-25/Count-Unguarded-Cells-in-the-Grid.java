65271541765ff8dba23ed516e96112667135c0ae
Pothamsetti Sai Srujan Reddy <157013684+SaiSrujanReddyP@users.noreply.github.com>
2025-11-02 23:48:14 +0530

    [2257] [Time Beats: 56.93%] [Memory Beats: 5.11%] - LeetPush

---
+++ DCP-11-25/Count-Unguarded-Cells-in-the-Grid.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        int[][] grid = new int[m][n];
+        for (int[] w : walls) grid[w[0]][w[1]] = 2;
+        for (int[] g : guards) grid[g[0]][g[1]] = 3;
+
+        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        for (int[] g : guards) {
+            for (int[] d : dir) {
+                int r = g[0] + d[0], c = g[1] + d[1];
+                while (r >= 0 && r < m && c >= 0 && c < n && grid[r][c] != 2 && grid[r][c] != 3) {
+                    if (grid[r][c] == 0) grid[r][c] = 1;
+                    r += d[0];
+                    c += d[1];
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) cnt++;
+            }
+        }
+        return cnt;
+    }
+}
